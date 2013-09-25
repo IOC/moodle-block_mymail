3e8e41ab171cb4fc741912c550df4d0974913065
Marc Català <reskit@gmail.com>
2013-09-25 12:35:15 +0200

    Added spanish language

---
+++ lang/es/block_mymail.php
@@ -0,0 +1,12 @@
+<?php
+$string['compose'] = 'Redactar';
+$string['courses'] = 'Cursos';
+$string['drafts'] = 'Borradores';
+$string['inbox'] = 'Entrada';
+$string['labels'] = 'Etiquetas';
+$string['pluginname'] = 'Mi correo';
+$string['sentmail'] = 'Enviados';
+$string['starred'] = 'Destacados';
+$string['trash'] = 'Papelera';
+$string['mymail:addinstance'] = 'Añadir un nuevo bloque Mi correo';
+$string['mymail:myaddinstance'] = 'Añadir un nuevo bloque Mi correo a Mi página inicial';
